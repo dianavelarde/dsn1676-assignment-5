711a339ee28f25a25f28c52ea927f3a5fb637ac0
dianavelarde <van_di09@hotmail.com>
2015-03-02 13:42:48 -0500

    js done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-var $form = $('form');
-var $input = $('#to-do-item');
-var $list = $('.list');
+var $form = $('.form');
+var $list = $('.list')
+var $input = $('.input');
+var $li = $('<li>');
 
 
 $form.on('submit', function (e) {
     e.preventDefault();
     
-    var $li = $('<li>').html
-    var inputValue = $input.val();
-    var $input = $('<button class="close">X</button>');
+    var $li = $('<li>').html($input.val());
+    var $inputDl = $('<button class="close">X</button>');
+    
+    $inputDl.on('click', function () {
+        $li.remove('li');
+    });
+    
+    $list.on('click', 'li', function () {
+        $(this).addClass('complete');
+    });
+    
+    $li.append($inputDl);
+    $list.prepend($li);
+    $input.val('');
+    
+});
