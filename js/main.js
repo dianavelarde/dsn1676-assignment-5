5b1845ffb23cb5fcefe71e2e4871a50adb20495a
dianavelarde <van_di09@hotmail.com>
2015-03-02 08:20:36 -0500

    initial js code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 var $form = $('form');
 var $input = $('#to-do-item');
+var $list = $('.list');
 
+
+$form.on('submit', function (e) {
+    e.preventDefault();
+    
+    var $li = $('<li>').html
+    var inputValue = $input.val();
+    var $input = $('<button class="close">X</button>');
